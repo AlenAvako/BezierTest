fdd078966c54fb2aa5b8f4392a6bc1c2f0520aa3
AlenAvako <alen.madwatch@gmail.com>
2022-06-09 15:56:59 +0300

    Initial Commit

---
+++ BezierTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BezierTest
+//
+//  Created by Ален Авако on 09.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
